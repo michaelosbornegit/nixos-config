5d29937712ace3e7fd538c19795e742394c40b95
Mike Osborne <resonatortune@gmail.com>
2025-02-02 13:05:42 -0800

    wip: move bootloader to hardware configuration

--- hosts/stratus/configuration.nix
+++ hosts/stratus/configuration.nix
@@ -21,7 +21,7 @@
     # ./users.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
-    ./hardware-configuration.nix
+    ../vm/hardware-configuration.nix
   ];
 
   nixpkgs = {
@@ -68,10 +68,6 @@
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   networking.hostName = "stratus"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -109,20 +105,20 @@
 
   # MYEDIT Remove GNOME bloat, from https://nixos.wiki/wiki/GNOME
   environment.gnome.excludePackages = (with pkgs; [
-    atomix # puzzle game
-    cheese # webcam tool
-    epiphany # web browser
-    evince # document viewer
-    geary # email reader
+    gnome.atomix # puzzle game
+    gnome.cheese # webcam tool
+    gnome.epiphany # web browser
+    gnome.evince # document viewer
+    gnome.geary # email reader
     # gedit # text editor MYEDIT keep
-    gnome-characters
-    gnome-music
+    gnome.gnome-characters
+    gnome.gnome-music
     gnome-photos
     # gnome-terminal MYEDIT keep
     gnome-tour
-    hitori # sudoku game
-    iagno # go game
-    tali # poker game
+    gnome.hitori # sudoku game
+    gnome.iagno # go game
+    gnome.tali # poker game
     # totem # video player MYEDIT keep
   ]);
 
@@ -200,8 +196,8 @@
   };
 
   # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "resonatortune";
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "resonatortune";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
@@ -241,43 +237,43 @@
   # pathsToLink for zsh system package completion
   environment.pathsToLink = [ "/share/zsh" ];
 
-  # nvidia support from https://nixos.wiki/wiki/Nvidia
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
+  # # nvidia support from https://nixos.wiki/wiki/Nvidia
+  # # Enable OpenGL
+  # hardware.graphics = {
+  #   enable = true;
+  # };
+  # # Load nvidia driver for Xorg and Wayland
+  # services.xserver.videoDrivers = ["nvidia"];
+  # hardware.nvidia = {
+  #   # Modesetting is required.
+  #   modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   # Enable this if you have graphical corruption issues or application crashes after waking
+  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+  #   # of just the bare essentials.
+  #   powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+  #   # Use the NVidia open source kernel module (not to be confused with the
+  #   # independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of 
+  #   # supported GPUs is at: 
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+  #   # Only available from driver 515.43.04+
+  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
+  #   open = false;
 
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+  #   # Enable the Nvidia settings menu,
+	#   # accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
 
   # Enable the OpenSSH daemon.

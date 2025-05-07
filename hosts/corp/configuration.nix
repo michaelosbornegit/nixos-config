3b7de2cf8ec1a3c3b47ae06307fc1f0155a6d4a5
Michael Osborne <mosborne@westmonroe.com>
2025-05-07 15:37:10 -0700

    feat: add guest agent and rdp for corp

--- hosts/corp/configuration.nix
+++ hosts/corp/configuration.nix
@@ -90,6 +90,32 @@
     shell = pkgs.zsh;
   };
 
+  # guest agent
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;  # enable copy and paste between host and guest
+
+  # remote desktop
+  services.gnome.gnome-remote-desktop.enable = true;
+  
+  services.xrdp.enable = true;
+  services.xrdp.openFirewall = true;
+  services.xrdp.defaultWindowManager = "gnome-session";
+  
+  # Add gnome-remote-desktop to system packages
+  environment.systemPackages = with pkgs; [
+    gnome-remote-desktop
+    gnome-session
+    xrdp
+  ];
+
+
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
 }

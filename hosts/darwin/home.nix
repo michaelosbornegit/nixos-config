b150a1fff41750ce7413bae7e7a380c8994d2702
Michael Osborne <mosborne@wblankmetal.ai>
2025-08-13 07:58:22 -0500

    feat: new darwin config, update email, add slack

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -7,7 +7,7 @@
   # overwrite some things for darwin
   home.homeDirectory = "/Users/${user}";
   # work email
-  programs.git.userEmail = "mosborne@westmonroe.com";
+  programs.git.userEmail = "mosborne@wblankmetal.ai";
 
   home.file = {
     ".p10k-config".source = ../../dotfiles/.p10k-config;
@@ -18,7 +18,8 @@
   };
 
   home.packages = [
-    pkgs.ollama
+    # pkgs.microsoft-edge If this is ever available for arm64 darwin
+    pkgs.slack
     pkgs.claude-code
     pkgs.awscli2
     pkgs.postman

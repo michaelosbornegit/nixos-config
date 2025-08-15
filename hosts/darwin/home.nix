ef6337aad6fee4eb075b58656566b3b3814e08f4
Michael Osborne <mosborne@wblankmetal.ai>
2025-08-15 15:29:32 -0500

    feat: update darwin claude code config

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -13,10 +13,6 @@
     ".p10k-config".source = ../../dotfiles/.p10k-config;
   };
 
-  home.sessionVariables = {
-    CLAUDE_CODE_USE_BEDROCK = "1";
-  };
-
   home.packages = [
     # pkgs.microsoft-edge If this is ever available for arm64 darwin
     pkgs.slack

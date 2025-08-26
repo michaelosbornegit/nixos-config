78dd6219e17a16a1f572eb696c04bce4f4787f1e
Michael Osborne <mosborne@blankmetal.ai>
2025-08-26 11:17:00 -0500

    feat: fix typo in git email, add firefox to darwin and notion and coreutils

---
+++ scripts/update-upgrade-darwin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is used to update and upgrade the darwin system.
+
+# For sudo 
+sudo echo hi
+
+sudo nix-collect-garbage -d
+
+nix flake update
+
+sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake .#darwin
+
+sudo reboot

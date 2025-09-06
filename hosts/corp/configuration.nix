5a3bdee4043de22ce47b77f3213e3fece757f447
Michael Osborne <mosborne@blankmetal.ai>
2025-09-06 12:33:42 -0500

    chore: update remaining state versions

--- hosts/corp/configuration.nix
+++ hosts/corp/configuration.nix
@@ -118,5 +118,5 @@
   systemd.targets.hybrid-sleep.enable = false;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05";
 }
--- hosts/home-common.nix
+++ hosts/home-common.nix
@@ -92,5 +92,5 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.11";
+  home.stateVersion = "25.05";
 }

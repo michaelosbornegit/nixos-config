3874f99c18465b0cdef174dacaaac510bf945e72
Michael Osborne <mosborne@blankmetal.ai>
2025-09-06 11:57:31 -0500

    feat: 24.11 EOL and add nixpkgs-unstable for bleeding edge packages faster than nixos-unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,10 +12,19 @@
     # });
   };
 
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
+  # When applied, the stable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.stable'
   stable-packages = final: _prev: {
     stable = import inputs.nixpkgs-stable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+
+  # When applied, the bleeding edge nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.unstable'
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
       system = final.system;
       config.allowUnfree = true;
     };

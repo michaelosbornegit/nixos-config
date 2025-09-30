4b0a97e66fc7e300a2224b26e347bab5f338d619
Michael Osborne <resonatortune@gmail.com>
2025-09-29 19:38:05 -0500

    feat: add optimize to scripts

--- scripts/update-upgrade-stratus.sh
+++ scripts/update-upgrade-stratus.sh
@@ -5,10 +5,15 @@
 # For sudo 
 sudo echo hi
 
+# Clean anything up to make sure we have enough space 
 sudo nix-collect-garbage -d
 
 nix flake update
 
 sudo nixos-rebuild switch --flake .#stratus
+
+# Delete old generations and optimize the store
+sudo nix-collect-garbage -d
+sudo nix-store --optimize
 
 sudo reboot
